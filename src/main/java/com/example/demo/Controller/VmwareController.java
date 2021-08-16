e31a3e98dc44df50a3fbd622faf7d7e075832e8a
Bhaskar Verma <bhaskar.verma@oyorooms.com>
2021-08-16 18:03:08 +0530

    post api

--- src/main/java/com/example/demo/Controller/VmwareController.java
+++ src/main/java/com/example/demo/Controller/VmwareController.java
@@ -2,10 +2,7 @@ package com.example.demo.Controller;
 
 import com.example.demo.Models.BodyModel;
 import com.example.demo.Models.Shape;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
@@ -14,7 +11,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/results")
 public class VmwareController {
-    @GetMapping(value = "{id}")
+    @PostMapping("/add")
     private Map<String,Double> TaskId(
             HttpServletRequest httpServletRequest,
             @RequestBody BodyModel bodyModel){
